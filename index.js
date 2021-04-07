ef065a4ee937554b694663d28c623f7e89a0f27a
Mohith Shrivastava <mohith.shrivastava@salesforce.com>
2021-04-07 00:22:07 -0400

    slack app enhancements

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jsforce = require('jsforce'); //jsforce open source library to connect to 
 
 // Create Salesforce Connection
 const oauth2 = new jsforce.OAuth2({
-    // you can change loginUrl to connect to sandbox or prerelease env.
+    // you can change loginUrl to connect to sandbox or scratchorg env.
     // loginUrl : 'https://test.salesforce.com',
     clientId: process.env.SALESFORCE_CLIENT_ID,
     clientSecret: process.env.SALESFORCE_CLIENT_SECRET,
@@ -11,7 +11,11 @@ const oauth2 = new jsforce.OAuth2({
 });
 const connection = new jsforce.Connection({ oauth2: oauth2 });
 
+// This is hack at this point. This needs to be stored in Database
+let oAuthResponseFromSlackInstall = {};
+
 // Create a Reciever for Installation and OAuth with Slack and Salesforce
+// One needs ExpressReciever if you want to take complete control of the OAuth 2.0 dance
 const receiver = new ExpressReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     clientId: process.env.SLACK_CLIENT_ID,
@@ -32,36 +36,11 @@ const receiver = new ExpressReceiver({
     installerOptions: {
         authVersion: 'v2', // default  is 'v2', 'v1' is used for classic slack apps
         installPath: '/slack/install',
-        redirectUriPath: '/slack/oauth_redirect',
         callbackOptions: {
             success: async (installation, installOptions, req, res) => {
                 try {
-                    await say({
-                        blocks: [
-                            {
-                                "type": "section",
-                                "text": {
-                                    "type": "mrkdwn",
-                                    "text": "To login into Salesforce click the button below"
-                                }
-                            },
-                            {
-                                "type": "actions",
-                                "elements": [
-                                    {
-                                        "type": "button",
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Authorize Salesforce"
-                                        },
-                                        "value": "authsfdc",
-                                        "action_id": "authsf"
-                                    }
-                                ]
-                            }
-                        ]
-                    })
-                    res.send('The app is successfully installed! You can close this window!!!!');
+                    oAuthResponseFromSlackInstall = installation;
+                    res.redirect('/slack/appinstall/success');
                 } catch (error) {
                     throw error;
                 }
@@ -71,7 +50,9 @@ const receiver = new ExpressReceiver({
                 res.send('failure');
             }
         }
-    },
+    }
+    // TODO Implement a install store to persist the tokens as per the below docs
+    // https://slack.dev/bolt-js/concepts#authenticating-oauth
 });
 
 // Instantiate Slack App with Custom Reciever
@@ -80,61 +61,117 @@ const app = new App({
     receiver
 });
 
+// Command to render button for Salesforce Connection
+app.command('/connectsf', async ({ command, ack, say }) => {
+    // Acknowledge command request
+    await ack();
+    const salesforce_url = `https://login.salesforce.com/services/oauth2/authorize?client_id=${process.env.SALESFORCE_CLIENT_ID}&redirect_uri=${process.env.SALESFORCE_REDIRECT_URL}&response_type=code`;
+    await say(renderAuthorizeButton(salesforce_url));
+});
+
+// Command to Acknowledge when Authorize Button is clicked
+app.action('authorize_sf', async ({ command, ack, say }) => {
+    await ack();
+});
+
+// Command to query Userinfo from Salesforce and display a Block UI
 app.command('/whoami', async ({ command, ack, say }) => {
     // Acknowledge command request
     await ack();
-    const userId = connection.userInfo.id;
-    const result = await connection.query(
-        `Select Id, Name, Phone, Email, Profile.Name FROM User WHERE ID='${userId}'`
-    );
-    console.log(result);
-    try {
-        await say({
-            blocks: [
-                {
-                    type: 'section',
-                    fields: [
-                        {
-                            type: 'mrkdwn',
-                            text: '*Name*'
-                        },
-                        {
-                            type: 'mrkdwn',
-                            text: '*Email*'
-                        },
-                        {
-                            type: 'plain_text',
-                            text: `${result.records[0].Name}`
-                        },
-                        {
-                            type: 'plain_text',
-                            text: `${result.records[0].Email}`
-                        },
-                        {
-                            type: 'mrkdwn',
-                            text: '*Phone*'
-                        },
-                        {
-                            type: 'mrkdwn',
-                            text: '*Profile Name*'
-                        },
-                        {
-                            type: 'plain_text',
-                            text: `${result.records[0].Phone}`
-                        },
-                        {
-                            type: 'plain_text',
-                            text: `${result.records[0].Profile.Name}`
-                        }
-                    ]
-                }
-            ]
-        });
-    } catch (e) {
-        console.log(e);
+    if (connection.userInfo) {
+        const userId = connection.userInfo.id;
+        const result = await connection.query(
+            `Select Id, Name, Phone, Email, Profile.Name FROM User WHERE ID='${userId}'`
+        );
+        console.log(result);
+        try {
+            await say(renderWhoamiBlock(result));
+        } catch (e) {
+            console.log(e);
+        }
+    } else {
+        const salesforce_url = `https://login.salesforce.com/services/oauth2/authorize?client_id=${process.env.SALESFORCE_CLIENT_ID}&redirect_uri=${process.env.SALESFORCE_REDIRECT_URL}&response_type=code`;
+        await say(renderAuthorizeButton(salesforce_url));
     }
 });
 
+// renders Whoami Block
+function renderWhoamiBlock(result) {
+    return {
+        blocks: [
+            {
+                type: 'section',
+                fields: [
+                    {
+                        type: 'mrkdwn',
+                        text: '*Name*'
+                    },
+                    {
+                        type: 'mrkdwn',
+                        text: '*Email*'
+                    },
+                    {
+                        type: 'plain_text',
+                        text: `${result.records[0].Name}`
+                    },
+                    {
+                        type: 'plain_text',
+                        text: `${result.records[0].Email}`
+                    },
+                    {
+                        type: 'mrkdwn',
+                        text: '*Phone*'
+                    },
+                    {
+                        type: 'mrkdwn',
+                        text: '*Profile Name*'
+                    },
+                    {
+                        type: 'plain_text',
+                        text: `${result.records[0].Phone}`
+                    },
+                    {
+                        type: 'plain_text',
+                        text: `${result.records[0].Profile.Name}`
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+// Block SDK for returning the Authorize Button
+function renderAuthorizeButton(salesforce_url) {
+    return {
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: 'To login into Salesforce click the button below'
+                }
+            },
+            {
+                type: 'actions',
+                elements: [
+                    {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: 'Authorize Salesforce'
+                        },
+                        style: 'primary',
+                        value: 'authsf',
+                        url: salesforce_url,
+                        action_id: 'authorize_sf'
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+// Upon redirect Authorize Salesforce
 receiver.router.get('/salesforce/oauth_redirect', async (req, res) => {
     let code = req.query.code;
     connection.authorize(code, function (err, userInfo) {
@@ -148,11 +185,27 @@ receiver.router.get('/salesforce/oauth_redirect', async (req, res) => {
         console.log(connection.instanceUrl);
         console.log('User ID: ' + userInfo.id);
         console.log('Org ID: ' + userInfo.organizationId);
-        // ...
-        res.send(
-            'Successfully connected slack with your Salesforce User. You can close this window'
-        ); // or your desired response
+        res.redirect('/auth/success');
+        // or your desired response
     });
+});
+
+// Custom Route for success Message
+receiver.router.get('/auth/success', async (req, res) => {
+    await app.client.chat.postMessage({'channel': oAuthResponseFromSlackInstall.incomingWebhook.channelId, 'text': 'Succcessfully authorized to Salesforce 🎉', token: oAuthResponseFromSlackInstall.bot.token});
+    res.send(
+        'Successfully connected slack with your Salesforce User. You can close this window'
+    );
+});
+
+// Custom Route for Install success Message
+receiver.router.get('/slack/appinstall/success', async (req, res) => {
+    await app.client.chat.postMessage({'channel': oAuthResponseFromSlackInstall.incomingWebhook.channelId, 'text': 'The app is Succcessfully Installed 🎊', token: oAuthResponseFromSlackInstall.bot.token});
+    res.send('The app is Successfully installed!!! You can close this window');
+});
+
+app.event('app_home_opened', ({ event, say }) => {  
+    say(`Slack Salesforce Hello App, <@${event.user}>!`);
 });
 
 (async () => {
